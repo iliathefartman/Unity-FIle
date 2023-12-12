b829671b3174166dd9faf04b130075c396e6aa3f
iliathefartman <iliamolaei09@gmail.com>
2023-12-12 15:39:16 -0500

    frf
    l

---
+++ ZZZ/Assets/Scripts/joe.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joe : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+
+    }
+
+    
+
+
+    
+}
---
+++ ZZZ/Assets/hidee.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class hidee : MonoBehaviour
+{
+
+    public GameObject background;
+
+    void Update()
+    {
+        background.active = false;
+
+    }
+}
